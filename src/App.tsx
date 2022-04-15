ab076a55d315bbc1cb54ef7d54075ccfc62fe1a0
TZawalich <t.zawalich@gmail.com>
2022-04-14 22:54:12 -0700

    grid applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css"
+
 import Header from './Header';
 import Footer from './Footer';
 import DiceRoller from './components/DiceRoller';
@@ -13,8 +14,23 @@ function App() {
   return (
     <div className="App">
       <Header />
-        <DiceRoller dice={4} passRoll={passRollHandler}/>
-
+        <div className={styles.bodyGrid}>
+          <div className={styles.columnTitles}>
+            <h2>Dice Number</h2>
+            <h2>Modifier +/-</h2>
+            <h2>Modifier</h2>
+            <h2>Modifier Type</h2>
+            <h2>Roll Dice</h2>
+            <h2>Result</h2>
+          </div>
+          <DiceRoller dice={4} passRoll={passRollHandler}/>
+          <DiceRoller dice={6} passRoll={passRollHandler}/>
+          <DiceRoller dice={8} passRoll={passRollHandler}/>
+          <DiceRoller dice={10} passRoll={passRollHandler}/>
+          <DiceRoller dice={12} passRoll={passRollHandler}/>
+          <DiceRoller dice={20} passRoll={passRollHandler}/>
+          <DiceRoller dice={100} passRoll={passRollHandler}/>
+        </div>
       <Footer />
     </div>
   );
--- src/components/DiceRoller.tsx
+++ src/components/DiceRoller.tsx
@@ -31,52 +31,111 @@ const DiceRoller = (props: { dice: number, passRoll: Function }) => {
         setModifierApplication(e.target.value);
     }
 
-    const rollHandler = (dice:number, numberOfDice:number|string, modifier:number|string, modifierDirection:string, modifierApplication:string) => {
+    const rollHandler = (dice: number, numberOfDice: number | string, modifier: number | string, modifierDirection: string, modifierApplication: string) => {
         let diceNumberCheck: number;
-        let modifierCheck : number;
+        let modifierCheck: number;
         let rolls = [];
+        let modifiedRolls: number[];
 
-        diceNumberCheck = isNaN(+numberOfDice) ? 0 :  +numberOfDice; //check if not a number, if string, set to zero, otherwise set to diceNumberCheck var for futher checks
+        diceNumberCheck = isNaN(+numberOfDice) ? 0 : +numberOfDice; //check if not a number, if string, set to zero, otherwise set to diceNumberCheck var for futher checks
         modifierCheck = isNaN(+modifier) ? 0 : +modifier;
 
         diceNumberCheck = diceNumberCheck > 500 ? 500 : diceNumberCheck; //stop these kids from rolling more than 500 dice at once
         modifierCheck = modifierDirection === "+" ? modifierCheck : modifierCheck * -1; //sets modifier direction
-        
-        if( modifierApplication === "all"){  //apply modifier roll to each die
-            for (let i = 0; i < diceNumberCheck; i++) { rolls.push(minMaxRoller(1, dice, modifierCheck)) }
-        }else{//apply modifier once to entire roll array
-            for (let i = 0; i < diceNumberCheck; i++) { rolls.push(minMaxRoller(1, dice, 0)) }
-            rolls.push(modifierCheck)
+
+        if (modifierApplication === "all") {  //apply modifier roll to each die
+            for (let i = 0; i < diceNumberCheck; i++) { rolls.push(minMaxRoller(1, dice)) }
+            modifiedRolls = rolls.map(x => x + modifierCheck)
+        } else {//apply modifier once to entire roll array
+            for (let i = 0; i < diceNumberCheck; i++) { rolls.push(minMaxRoller(1, dice)) }
+            modifiedRolls = [...rolls, modifierCheck]
         }
-        const result = rolls.reduce((acc, val) => acc + val, 0);
+
+        const result = modifiedRolls.reduce((acc, val) => acc + val, 0);
 
         setResults(result);
-        props.passRoll({result, rolls, modifierApplication, })
+        props.passRoll({ result, rolls, modifierApplication, modifier })
     }
 
-    function minMaxRoller(min: number, max: number, modifier: number) {
-        return (min + Math.floor(Math.random() * (max - min + 1)) + modifier)
+    function minMaxRoller(min: number, max: number) {
+        return (min + Math.floor(Math.random() * (max - min + 1)))
     }
 
     return (
         <div className={styles.diceRow}>
             <div className={styles.numberOfDice}>
-                <input type="number" value={numberOfDice} min={0} max={500} placeholder="0" onChange={setNumberHandler}></input> <label>d{dice}</label>
+                <label>
+                    <input
+                        type="number"
+                        value={numberOfDice}
+                        min={0} max={500}
+                        placeholder="0"
+                        onChange={setNumberHandler}
+                    />
+                    d{dice}
+                </label>
             </div>
             <div className={styles.modifierDirection}>
-                <div onChange={setModifierDirectionHandler}>
-                    <input type="radio" value="+" name="+" checked={modifierDirection === "+"} /> +
-                    <input type="radio" value="-" name="-" checked={modifierDirection === "-"} /> -
-                </div>
-            </div>
-            <div className={styles.modifierApplication}>
-                <div onChange={setModifierApplicationHandler}>
-                    <input type="radio" value="all" name="all" checked={modifierApplication === "all"} /> Add modifier to each die ex. 5(d4+1)
-                    <input type="radio" value="once" name="once" checked={modifierApplication === "once"} /> Add modifier after rolling ex. (5d4)+1
-                </div>
+                <form>
+                    <label>
+                        <input
+                            type="radio"
+                            value="+"
+                            name="+"
+                            onChange={setModifierDirectionHandler}
+                            checked={modifierDirection === "+"}
+                        />
+                        +
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="-"
+                            name="-"
+                            onChange={setModifierDirectionHandler}
+                            checked={modifierDirection === "-"}
+                        />
+                        -
+                    </label>
+                </form>
             </div>
             <div className={styles.modifier}>
-                <input type="number" value={modifier} min={0} max={500} placeholder="0" onChange={setModifierHandler}></input> <label>Modifier</label>
+                <label>
+                    <input
+                        type="number"
+                        value={modifier}
+                        min={0}
+                        max={500}
+                        placeholder="0"
+                        onChange={setModifierHandler}
+                    />
+                    Modifier
+                </label>
+
+            </div>
+            <div className={styles.modifierApplication}>
+                <form>
+                    <label>
+                        <input
+                            type="radio"
+                            value="all"
+                            name="all"
+                            onChange={setModifierApplicationHandler}
+                            checked={modifierApplication === "all"}
+                        />
+                        Add modifier to each die ex. 5(d4+1)
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="once"
+                            name="once"
+                            onChange={setModifierApplicationHandler}
+                            checked={modifierApplication === "once"}
+                        />
+                        Add modifier after rolling ex. (5d4)+1
+                    </label>
+                </form>
             </div>
             <div className={styles.roll}>
                 <button onClick={() => rollHandler(dice, numberOfDice, modifier, modifierDirection, modifierApplication)}>Roll</button>
